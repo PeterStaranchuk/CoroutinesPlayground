8a62afa9f2206554316f7dcb6a33aca5d4b666c2
peterstaranchuk <petr.staranchuk@gmail.com>
2020-01-28 23:00:01 +0300

    Practiced error handling in coroutines

--- app/src/main/java/com/peterstaranchuk/coroutinesplaygraung/MainActivity.kt
+++ app/src/main/java/com/peterstaranchuk/coroutinesplaygraung/MainActivity.kt
@@ -2,13 +2,65 @@ package com.peterstaranchuk.coroutinesplaygraung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope{
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
+
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("Caught $exception")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        launch {
+            try {
+                login()
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+        }
 
+        val userInfo = async {
+            fetchUserInfo()
+        }
+
+        launch {
+            try {
+                userInfo.await()
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        GlobalScope.launch(handler) {
+            try {
+                login()
+                userInfo.await()
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+        }
     }
+
+    private suspend fun login() {
+        delay(2000)
+        Log.d("TAG", "User logged")
+        throw IllegalStateException("User not exist")
+    }
+
+    private suspend fun fetchUserInfo() : UserInfo {
+        throw IllegalStateException("User info cannot be fetched")
+        return UserInfo("Peter", 28)
+    }
+
+    data class UserInfo(val name : String, val age : Int)
 }
